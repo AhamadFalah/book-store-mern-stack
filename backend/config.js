d1b1f630a2d76cdf5b508a14184084cf97be8ae1
falahahamad <ahamadfalah.dev@gmail.com>
2023-12-16 09:16:38 +0530

    configure MongoDB connection

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://root:root@book-store-mern-stack.2su2awb.mongodb.net/books-collection?retryWrites=true&w=majority';
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,25 @@
-import express from "express";
-import { PORT } from "./config.js"
+import express, { request, response } from "express";
+import { PORT } from "./config.js";
+import {mongoDBURL} from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`App listening to port: ${PORT}`);
+app.get('/',(request,response) =>{
+    console.log(request,)
+    return response.status(234).send('Welcome to MERN Stack Tutorial')
+})
+
+
+
+mongoose.connect(mongoDBURL).then(()=>{
+    console.log('App Successfully connected to DB');
+
+    app.listen(PORT, () => {
+        console.log(`App listening to port: ${PORT}`);
+    });
+    
+}).catch((error)=>{
+    console.log(error);
 });
 
