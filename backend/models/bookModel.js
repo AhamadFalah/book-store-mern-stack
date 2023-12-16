c4a91748f2e84c8588ed69076cf9cd5d0466db04
ahamadfalah <it22885296@my.sliit.lk>
2023-12-16 09:40:19 +0530

    create book model

---
+++ backend/models/bookModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const bookSchema = mongoose.Schema(
+    {
+        Title:{
+            type: String,
+            required: true,
+        },
+        Author:{
+            type: String,
+            required: true,
+        },
+        PublishYear:{
+            type: Number,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Book = mongoose.model("Book", bookSchema);
