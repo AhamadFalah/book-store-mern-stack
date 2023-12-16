c2a3d6d9ef8b5a6073a44c628a5a9daf8202b400
ahamadfalah <it22885296@my.sliit.lk>
2023-12-16 09:22:52 +0530

    refactor config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,4 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 'mongodb+srv://root:root@book-store-mern-stack.2su2awb.mongodb.net/books-collection?retryWrites=true&w=majority';
+export const mongoDBURL =
+  "mongodb+srv://root:root@book-store-mern-stack.2su2awb.mongodb.net/books-collection?retryWrites=true&w=majority";
