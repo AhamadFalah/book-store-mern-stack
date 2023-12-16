b2f77f587d710e5abb99d222afc060aadd68564a
ahamadfalah <it22885296@my.sliit.lk>
2023-12-16 09:23:23 +0530

    refactor index.js

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,24 @@
 import express, { request, response } from "express";
 import { PORT } from "./config.js";
-import {mongoDBURL} from "./config.js";
+import { mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 
 const app = express();
 
-app.get('/',(request,response) =>{
-    console.log(request,)
-    return response.status(234).send('Welcome to MERN Stack Tutorial')
-})
-
-
-
-mongoose.connect(mongoDBURL).then(()=>{
-    console.log('App Successfully connected to DB');
-
-    app.listen(PORT, () => {
-        console.log(`App listening to port: ${PORT}`);
-    });
-    
-}).catch((error)=>{
-    console.log(error);
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome to MERN Stack Tutorial");
 });
 
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App Successfully connected to DB");
+
+    app.listen(PORT, () => {
+      console.log(`App listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
